a78a376aaf673590b5c2634e63b59b8d741f6ecf
GerBor <gerard.boruszewicz@gmail.com>
2026-02-15 23:39:28 +0100

    Fixed entropy_mixer

--- ro_trng/ro_trng.gen/sources_1/bd/top_level_design/hdl/top_level_design_wrapper.v
+++ ro_trng/ro_trng.gen/sources_1/bd/top_level_design/hdl/top_level_design_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
-//Date        : Sat Feb 14 22:24:50 2026
+//Date        : Sun Feb 15 21:03:51 2026
 //Host        : DESKTOP-L7RS5J6 running 64-bit major release  (build 9200)
 //Command     : generate_target top_level_design_wrapper.bd
 //Design      : top_level_design_wrapper
@@ -12,18 +12,14 @@
 
 module top_level_design_wrapper
    (CLK100MHZ,
-    led0,
     uart_tx);
   input CLK100MHZ;
-  output [0:0]led0;
   output uart_tx;
 
   wire CLK100MHZ;
-  wire [0:0]led0;
   wire uart_tx;
 
   top_level_design top_level_design_i
        (.CLK100MHZ(CLK100MHZ),
-        .led0(led0),
         .uart_tx(uart_tx));
 endmodule
--- ro_trng/ro_trng.srcs/sources_1/new/entropy_mixer.vhd
+++ ro_trng/ro_trng.srcs/sources_1/new/entropy_mixer.vhd
@@ -3,28 +3,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity entropy_mixer is
     port (
-        clk      : in  std_logic;
-        rst_l    : in  std_logic;
-        ro_sync  : in  std_logic_vector(7 downto 0);
-        raw_bit  : out std_logic
+        clk       : in  std_logic;
+        rst_l     : in  std_logic;
+        ro_sync   : in  std_logic_vector(7 downto 0);
+        bit_out   : out std_logic
     );
-end entropy_mixer;
+end;
 
 architecture RTL of entropy_mixer is
-    signal xor_reg : std_logic;
+    signal raw_bit    : std_logic;
+    signal prev_bit   : std_logic;
 begin
 
-    process(clk, rst_l)
-        variable tmp : std_logic;
+    raw_bit <= ro_sync(0) xor ro_sync(1) xor ro_sync(2) xor ro_sync(3)
+            xor ro_sync(4) xor ro_sync(5) xor ro_sync(6) xor ro_sync(7);
+
+    process(clk)
     begin
-        if rst_l = '0' then
-            xor_reg <= '0';
-        elsif rising_edge(clk) then
-            tmp := ro_sync(0) xor ro_sync(1) xor ro_sync(2) xor ro_sync(3) xor ro_sync(4) xor ro_sync(5) xor ro_sync(6) xor ro_sync(7);
-            xor_reg <= tmp;
+        if rising_edge(clk) then
+            if rst_l = '0' then
+                prev_bit <= '0';
+            else
+                prev_bit <= raw_bit;
+            end if;
         end if;
     end process;
 
-    raw_bit <= xor_reg;
+    bit_out <= raw_bit xor prev_bit;
 
 end RTL;
